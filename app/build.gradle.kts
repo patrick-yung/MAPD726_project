95f0a08020d216b93663b455aba0820da5498e26
Patrick Yung <patrickyyung@gmail.com>
2026-02-09 12:01:28 -0500

    Added/test backend server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
