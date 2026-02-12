c3fb280cdb1f92c43a3d36ccdea4da61f656028b
Iffat <iamagduri@gmail.com>
2026-02-12 13:15:14 -0500

    created the main homeScreen

--- app/src/main/java/com/example/yungpakhongpatrick/mapd726_project/MainActivity.kt
+++ app/src/main/java/com/example/yungpakhongpatrick/mapd726_project/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.yungpakhongpatrick.mapd726_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.yungpakhongpatrick.mapd726_project.R.*
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
